ff1abfaf6070e74dbb7605fba532686fd194d861
Suprem Vanam <supremvanam.gsp@gmail.com>
2023-02-04 14:08:46 -0500

    Update MY_SCHEDULE_DATE after a successful reschedule to avoid checking with the old date

--- visa.py
+++ visa.py
@@ -18,7 +18,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -26,7 +25,7 @@ USERNAME = config['USVISA']['USERNAME']
 PASSWORD = config['USVISA']['PASSWORD']
 SCHEDULE_ID = config['USVISA']['SCHEDULE_ID']
 MY_SCHEDULE_DATE = config['USVISA']['MY_SCHEDULE_DATE']
-COUNTRY_CODE = config['USVISA']['COUNTRY_CODE'] 
+COUNTRY_CODE = config['USVISA']['COUNTRY_CODE']
 FACILITY_ID = config['USVISA']['FACILITY_ID']
 
 SENDGRID_API_KEY = config['SENDGRID']['SENDGRID_API_KEY']
@@ -36,16 +35,19 @@ PUSH_USER = config['PUSHOVER']['PUSH_USER']
 LOCAL_USE = config['CHROMEDRIVER'].getboolean('LOCAL_USE')
 HUB_ADDRESS = config['CHROMEDRIVER']['HUB_ADDRESS']
 
-REGEX_CONTINUE = "//a[contains(text(),'Continuar')]"
+# Text on the button after logging in. Change it to your language if it's not English
+# Example: Continuar, if Spanish.
+REGEX_CONTINUE = "//a[contains(text(),'Continue')]"
 
 
 # def MY_CONDITION(month, day): return int(month) == 11 and int(day) >= 5
-def MY_CONDITION(month, day): return True # No custom condition wanted for the new scheduled date
+def MY_CONDITION(month, day): return True  # No custom condition wanted for the new scheduled date
+
 
 STEP_TIME = 0.5  # time between steps (interactions with forms): 0.5 seconds
-RETRY_TIME = 60*10  # wait time between retries/checks for available dates: 10 minutes
-EXCEPTION_TIME = 60*30  # wait time when an exception occurs: 30 minutes
-COOLDOWN_TIME = 60*60  # wait time when temporary banned (empty list): 60 minutes
+RETRY_TIME = 60 * 5  # wait time between retries/checks for available dates: 5 minutes
+EXCEPTION_TIME = 60 * 30  # wait time when an exception occurs: 30 minutes
+COOLDOWN_TIME = 60 * 60  # wait time when temporary banned (empty list): 60 minutes
 
 DATE_URL = f"https://ais.usvisa-info.com/{COUNTRY_CODE}/niv/schedule/{SCHEDULE_ID}/appointment/days/{FACILITY_ID}.json?appointments[expedite]=false"
 TIME_URL = f"https://ais.usvisa-info.com/{COUNTRY_CODE}/niv/schedule/{SCHEDULE_ID}/appointment/times/{FACILITY_ID}.json?date=%s&appointments[expedite]=false"
@@ -87,6 +89,7 @@ def get_driver():
     else:
         dr = webdriver.Remote(command_executor=HUB_ADDRESS, options=webdriver.ChromeOptions())
     return dr
+
 
 driver = get_driver()
 
@@ -170,8 +173,11 @@ def reschedule(date):
     data = {
         "utf8": driver.find_element(by=By.NAME, value='utf8').get_attribute('value'),
         "authenticity_token": driver.find_element(by=By.NAME, value='authenticity_token').get_attribute('value'),
-        "confirmed_limit_message": driver.find_element(by=By.NAME, value='confirmed_limit_message').get_attribute('value'),
-        "use_consulate_appointment_capacity": driver.find_element(by=By.NAME, value='use_consulate_appointment_capacity').get_attribute('value'),
+        "confirmed_limit_message": driver.find_element(by=By.NAME, value='confirmed_limit_message').get_attribute(
+            'value'),
+        "use_consulate_appointment_capacity": driver.find_element(by=By.NAME,
+                                                                  value='use_consulate_appointment_capacity').get_attribute(
+            'value'),
         "appointments[consulate_appointment][facility_id]": FACILITY_ID,
         "appointments[consulate_appointment][date]": date,
         "appointments[consulate_appointment][time]": time,
@@ -184,8 +190,11 @@ def reschedule(date):
     }
 
     r = requests.post(APPOINTMENT_URL, headers=headers, data=data)
-    if(r.text.find('Successfully Scheduled') != -1):
+    if (r.text.find('Successfully Scheduled') != -1):
         msg = f"Rescheduled Successfully! {date} {time}"
+        global MY_SCHEDULE_DATE
+        MY_SCHEDULE_DATE = date
+        print("Updated MY_SCHEDULE_DATE with newly rescheduled date: ", MY_SCHEDULE_DATE)
         send_notification(msg)
         EXIT = True
     else:
@@ -195,7 +204,7 @@ def reschedule(date):
 
 def is_logged_in():
     content = driver.page_source
-    if(content.find("error") != -1):
+    if (content.find("error") != -1):
         return False
     return True
 
@@ -225,7 +234,7 @@ def get_available_date(dates):
         date = d.get('date')
         if is_earlier(date) and date != last_seen:
             _, month, day = date.split('-')
-            if(MY_CONDITION(month, day)):
+            if (MY_CONDITION(month, day)):
                 last_seen = date
                 return date
 
@@ -251,9 +260,9 @@ if __name__ == "__main__":
 
             dates = get_date()[:5]
             if not dates:
-              msg = "List is empty"
-              send_notification(msg)
-              EXIT = True
+                msg = "List is empty"
+                send_notification(msg)
+                EXIT = True
             print_dates(dates)
             date = get_available_date(dates)
             print()
@@ -262,21 +271,21 @@ if __name__ == "__main__":
                 reschedule(date)
                 push_notification(dates)
 
-            if(EXIT):
+            if (EXIT):
                 print("------------------exit")
                 break
 
             if not dates:
-              msg = "List is empty"
-              send_notification(msg)
-              #EXIT = True
-              time.sleep(COOLDOWN_TIME)
+                msg = "List is empty"
+                send_notification(msg)
+                # EXIT = True
+                time.sleep(COOLDOWN_TIME)
             else:
-              time.sleep(RETRY_TIME)
+                time.sleep(RETRY_TIME)
 
         except:
             retry_count += 1
             time.sleep(EXCEPTION_TIME)
 
-    if(not EXIT):
+    if (not EXIT):
         send_notification("HELP! Crashed.")
