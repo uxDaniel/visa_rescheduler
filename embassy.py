e9dcf25356b9e5fbc305dd7def3eab26f461f79a
Soroosh Noorzad <soroosh.noorzad@gmail.com>
2023-02-14 21:30:21 +0330

    Update embassy.py

--- embassy.py
+++ embassy.py
@@ -1,6 +1,8 @@
 # Embassy List
 Embassies = {
     # [EMBASSY (COUNTRY CODE), FACILITY_ID (EMBASSY ID), "Continue in different languages"],
-    "arm": ["en-am", 122, "Continue"], # English - Armenia - YEREVAN
-    "col": ["es-co", 25, "Continuar"], # Spanish - Colombia - Bogotá
+    "en-am-ye": ["en-am", 122, "Continue"], # English - Armenia - YEREVAN
+    "es-co-bo": ["es-co", 25, "Continuar"], # Spanish - Colombia - Bogotá
+    "en-ca-ot": ["en-ca", 0, "Continue"], # English - Canada - Ottawa
+    "en-ca-to": ["en-ca", 0, "Continue"], # English - Canada - Toronto
 }
