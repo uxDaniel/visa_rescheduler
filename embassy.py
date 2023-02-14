5f9a56df3809379cdadb6debd5dfd5785f5dcfe1
Soroosh Noorzad <soroosh.noorzad@gmail.com>
2023-02-14 22:03:39 +0330

    Update embassy.py

--- embassy.py
+++ embassy.py
@@ -2,7 +2,12 @@
 Embassies = {
     # [EMBASSY (COUNTRY CODE), FACILITY_ID (EMBASSY ID), "Continue in different languages"],
     "en-am-ye": ["en-am", 122, "Continue"], # English - Armenia - YEREVAN
-    "es-co-bo": ["es-co", 25, "Continuar"], # Spanish - Colombia - Bogotá
-    "en-ca-ot": ["en-ca", 0, "Continue"], # English - Canada - Ottawa
-    "en-ca-to": ["en-ca", 0, "Continue"], # English - Canada - Toronto
+    "es-co-bog": ["es-co", 25, "Continuar"], # Spanish - Colombia - Bogotá
+    "en-ca-cal": ["en-ca", 89, "Continue"], # English - Canada - Calgary
+    "en-ca-hal": ["en-ca", 90, "Continue"], # English - Canada - Halifax
+    "en-ca-mon": ["en-ca", 91, "Continue"], # English - Canada - Montreal
+    "en-ca-ott": ["en-ca", 92, "Continue"], # English - Canada - Ottawa
+    "en-ca-que": ["en-ca", 93, "Continue"], # English - Canada - Quebec City
+    "en-ca-tor": ["en-ca", 94, "Continue"], # English - Canada - Toronto
+    "en-ca-van": ["en-ca", 95, "Continue"], # English - Canada - Vancouver
 }
