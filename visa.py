fc08a80e4ff4048dc5d131f206396acc6a25aae3
Harold Lee <31803859+zttofficial@users.noreply.github.com>
2023-07-26 02:07:28 -0400

    Update visa.py
    newest version is not support WebDriverManager

--- visa.py
+++ visa.py
@@ -83,7 +83,7 @@ def send_notification(msg):
 
 def get_driver():
     if LOCAL_USE:
-        dr = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        dr = webdriver.Chrome(service=Service(ChromeDriverManager(version="114.0.5735.90").install()))
     else:
         dr = webdriver.Remote(command_executor=HUB_ADDRESS, options=webdriver.ChromeOptions())
     return dr
